e4db128c332ddfc52163dbbed518c0cc78ffec36
dontpkme <r9843003@ms.niu.edu.tw>
2014-09-28 18:41:27 +0800

    correct return type

--- src/net/dpkm/psm/handler/v1/MovieCommentHandler.java
+++ src/net/dpkm/psm/handler/v1/MovieCommentHandler.java
@@ -1,15 +1,16 @@
 package net.dpkm.psm.handler.v1;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.dpkm.psm.handler.ApiHandler;
+import net.dpkm.psm.model.Article;
 
 public class MovieCommentHandler extends ApiHandler {
 
 	@Override
 	public Object run(String queryString) {
-		Map result = new HashMap();
+		List<Article> result = new ArrayList<Article>();
 		return result;
 	}
 }
